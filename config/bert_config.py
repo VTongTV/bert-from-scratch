1ffad6a7b25c3fd236d1ad3958980547ed3e4556
VTongTV <vedant.tong@gmail.com>
2026-05-21 02:31:44 +0000

    config: add BERT model configuration dataclass

---
+++ config/bert_config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BertConfig:
+    L: int = 12
+    H: int = 768
+    A: int = 12
+    d_ff: int = 3072
+    V: int = 30000
+    max_len: int = 512
+    P_drop: float = 0.1
+
+    def __post_init__(self):
+        self.d_ff = 4 * self.H
