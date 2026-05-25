c95cc2e80177bc869939ed6aa7e3c87c9f624276
VTongTV <vedant.tong@gmail.com>
2026-05-25 02:29:12 +0000

    data: add MLM random token replacement (10%)

--- data/mlm.py
+++ data/mlm.py
@@ -7,3 +7,7 @@ def mask_token(token, rng, vocab_size, mask_prob=0.8):
     if rng.random() < mask_prob:
         return MASK
     return token
+
+
+def random_replace(token, rng, vocab_size):
+    return rng.randint(0, vocab_size - 1)
