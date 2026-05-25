bcdc6e7da691c0f3e059b3a3257b5416526136dd
VTongTV <vedant.tong@gmail.com>
2026-05-25 21:59:56 +0000

    data: add combined MLM masking procedure

--- data/mlm.py
+++ data/mlm.py
@@ -15,3 +15,17 @@ def random_replace(token, rng, vocab_size):
 
 def keep_unchanged(token, rng, vocab_size):
     return token
+
+
+def apply_mlm_mask(input_ids, rng, vocab_size, mlm_prob=0.15):
+    masked_ids = list(input_ids)
+    labels = [-1] * len(input_ids)
+    for i in range(len(input_ids)):
+        if rng.random() < mlm_prob:
+            labels[i] = input_ids[i]
+            r = rng.random()
+            if r < 0.8:
+                masked_ids[i] = vocab_size + 1
+            elif r < 0.9:
+                masked_ids[i] = rng.randint(0, vocab_size - 1)
+    return masked_ids, labels
