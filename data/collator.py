861175c11e3e8079fbec4ee26cdf3f84af3a1d73
VTongTV <vedant.tong@gmail.com>
2026-05-27 02:17:33 +0000

    data: add GLUE data collator

--- data/collator.py
+++ data/collator.py
@@ -9,3 +9,11 @@ def pretrain_collate(batch):
     mlm_labels = torch.tensor([e.mlm_labels for e in batch], dtype=torch.long)
     nsp_labels = torch.tensor([e.nsp_label for e in batch], dtype=torch.long)
     return input_ids, segment_ids, attention_mask, masked_ids, mlm_labels, nsp_labels
+
+
+def glue_collate(batch):
+    input_ids = torch.tensor([f.input_ids for f in batch], dtype=torch.long)
+    segment_ids = torch.tensor([f.segment_ids for f in batch], dtype=torch.long)
+    attention_mask = torch.tensor([f.attention_mask for f in batch], dtype=torch.long)
+    labels = torch.tensor([f.label for f in batch], dtype=torch.long)
+    return input_ids, segment_ids, attention_mask, labels
