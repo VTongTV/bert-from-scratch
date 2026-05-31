0a0861a03a651e4c8d7f86a0fac5d58b7ad34e37
VTongTV <vedant.tong@gmail.com>
2026-05-31 02:07:58 +0000

    model: add BERT forward pass

--- model/bert.py
+++ model/bert.py
@@ -14,3 +14,11 @@ class BertModel(nn.Module):
         self.embeddings = BertEmbeddings(config.V, config.H, config.max_len)
         self.encoder = BertEncoder(config.L, config.H, config.A, config.d_ff, config.P_drop)
         self.pooler = BertPooler(config.H)
+
+    def forward(self, input_ids, segment_ids=None, attention_mask=None):
+        if attention_mask is not None:
+            attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+        embedding_output = self.embeddings(input_ids, segment_ids)
+        encoder_output = self.encoder(embedding_output, attention_mask)
+        pooled_output = self.pooler(encoder_output)
+        return encoder_output, pooled_output
