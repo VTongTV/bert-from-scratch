9c0ce3f7d0d6ef4086f6201a6ec747a6b3a14ee7
VTongTV <vedant.tong@gmail.com>
2026-06-05 00:59:21 +0000

    model: add NSP head (pooler output + linear)

---
+++ model/nsp_head.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class NSPHead(nn.Module):
+    def __init__(self, H):
+        super().__init__()
+        self.classifier = nn.Linear(H, 2)
+
+    def forward(self, pooled_output):
+        return self.classifier(pooled_output)
+
+
+def nsp_loss(logits, labels):
+    return F.cross_entropy(logits, labels)
